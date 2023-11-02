a6a6adfb779077a6a59785eab40ba08a489bc2d0
whs <hswongac@gmail.com>
2023-11-02 21:15:52 +0800

    Refactor

--- comb/laptop/nixosConfigurations.nix
+++ comb/laptop/nixosConfigurations.nix
@@ -1,17 +1,16 @@
 {
   x17 = {
-    bee.system = "x86_64-linux";
-    bee.pkgs = cell.pkgs;
-    bee.home = inputs.home-manager;
-
     imports = [
       cell.diskoConfigurations.x17
       cell.hardwareProfiles.x17
       cell.homeConfigurations.x17
       cell.nixosProfiles.x17
       inputs.disko.nixosModules.disko
+      {
+        bee.home = inputs.home-manager;
+        bee.pkgs = cell.pkgs;
+        bee.system = "x86_64-linux";
+      }
     ];
-
-    system.stateVersion = "23.05";
   };
 }
